ed3e2b01c6581508a8cd1b847a86183915ae39e2
kiethuynh31 <kietminhlop51.z@gmail.com>
2025-10-04 07:48:51 +0700

    Add files via upload
    Lab3

---
+++ src/test/java/com/example/TS01297Lab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TS01297Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
